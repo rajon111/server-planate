66a81c8ea6f4785d0395ac9f46bfadee745aba6f
imhasibulhasan <hashibul218@gmail.com>
2022-07-27 12:43:29 +0600

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home/home.component';
+import { HomeSliderComponent } from './components/pages/home/home-slider/home-slider.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeSliderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
